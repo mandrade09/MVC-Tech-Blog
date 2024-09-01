5c99c1cf7002d451e18ac1adffd7cda68e14fd5b
Matthew  Andrade <mattandrade09@gmail.com>
2024-08-31 23:59:29 -0500

    updated styling

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 const express = require('express');
+const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
 const userRoutes = require('./routes/api/userRoutes');
 const hbs = exphbs.create({});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+});
+app.use(session({
+  secret: 'super secret',
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore,
+}));
 
-app.use(session(sess));
+app.use(express.static('public'));
 
+// Set up Handlebars.js
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
+// Routes
 app.use(require('./routes/api/homeRoutes'));
 app.use('/api/users', require('./routes/api/userRoutes'));
+app.use('/api/posts', require('./routes/api/postRoutes'));
+app.use('/api/comments', require('./routes/api/commentRoutes'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
