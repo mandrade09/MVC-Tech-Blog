a9f0f5b7b3a3e3d39b42392382c22cce5a999416
Matthew  Andrade <mattandrade09@gmail.com>
2024-09-12 08:44:43 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,19 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres', // Make sure this is set to 'postgres'
-    port: 5432, // Default PostgreSQL port
-  }
-);
+let sequelize;
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
 
 module.exports = sequelize;
